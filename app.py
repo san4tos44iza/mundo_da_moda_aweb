02c0c37ed7c16e45d0e060140533cf8850c66c50
Eloiza Santos <147550053+san4tos44iza@users.noreply.github.com>
2026-02-02 16:12:30 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request, redirect
+import MySQLdb
+
+app = Flask(__name__)
+
+db = MySQLdb.connect(
+    host="127.0.0.1",
+    user="root",
+    passwd="labinfo",
+    db="moda"
+)
+
+@app.route("/")
+def index():
+    return render_template("login.html")
+
+
+@app.route("/index")
+def login():
+    return render_template("index.html")
+
+
+@app.route("/registrar", methods=["POST"])
+def registrar():
+    nome = request.form["username"]
+    email = request.form["email"]
+    senha = request.form["password"]
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM usuarios WHERE email = %s", (email,))
+    usuario = cursor.fetchone()
+
+    if usuario:
+        return redirect("/index")
+
+    sql = "INSERT INTO usuarios (nome, email, senha) VALUES (%s, %s, %s)"
+    valores = (nome, email, senha)
+
+    cursor.execute(sql, valores)
+    db.commit()
+
+    return redirect("/index")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
