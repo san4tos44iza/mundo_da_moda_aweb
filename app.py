7931dee764282593217201c32a2a49ee849747ae
Eloiza Santos <147550053+san4tos44iza@users.noreply.github.com>
2026-02-02 16:15:28 -0300

    Update app.py
    mudança no codigo

--- app.py
+++ app.py
@@ -11,13 +11,13 @@ db = MySQLdb.connect(
 )
 
 @app.route("/")
-def index():
-    return render_template("login.html")
-
-
-@app.route("/index")
-def login():
+def paginaprincipal():
     return render_template("index.html")
+
+
+@app.route("/paginaprincipal")
+def index():
+    return render_template("paginaprincipal.html")
 
 
 @app.route("/registrar", methods=["POST"])
@@ -32,7 +32,7 @@ def registrar():
     usuario = cursor.fetchone()
 
     if usuario:
-        return redirect("/index")
+        return redirect("/paginaprincipal")
 
     sql = "INSERT INTO usuarios (nome, email, senha) VALUES (%s, %s, %s)"
     valores = (nome, email, senha)
@@ -40,7 +40,7 @@ def registrar():
     cursor.execute(sql, valores)
     db.commit()
 
-    return redirect("/index")
+    return redirect("/paginaprincipal")
 
 
 if __name__ == "__main__":
